6cc6e1e1e39a1a2f5e9246baf8fdfbe29425e167
Umutonipamera <umutonipamera@gmail.com>
2021-02-21 00:02:20 +0300

    Assignment1

---
+++ src/Assignment.kt
@@ -0,0 +1,14 @@
+fun main() {
+    var studentName ="Pamera Umutoni"
+    println(studentName)
+    var age=22
+    println(age)
+    var phoneNumber="0786397732"
+    println(phoneNumber)
+    var weight=60
+    println(weight)
+    var citizen ="Kenya"
+    println(citizen)
+    var kenyancitizen: Boolean= false
+    println(kenyancitizen)
+}
